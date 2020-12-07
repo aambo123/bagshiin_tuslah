40744e500c34ac7c635bff28e8f968f7bbbc6d22
amboavan <46276810+amboavan@users.noreply.github.com>
2020-12-07 22:33:16 +0800

    add list page and fix menuy

--- js/common.js
+++ js/common.js
@@ -21,5 +21,51 @@ fetch("/html/template/head.html")
     })
     .then(function (data) {
         document.querySelector("head").innerHTML = data;
+        duplicate('dropWrap', 'dropItem', 3)
+        var dropDownButton = document.querySelectorAll('[data-toggle=dropdown]');
+        
+        Object.keys(dropDownButton).forEach.call(dropDownButton, (key,i) =>{
+            dropDownButton[i].addEventListener('click', (event) =>{
+                event.preventDefault();
+                var trgt = event.currentTarget.dataset.target;
+                var object = document.querySelector(trgt);
+                
+                object.classList.toggle('open')
+    
+            })
+        })
+
+        var scrollPosition = window.scrollY;
+        var nav = document.getElementById('nav');
+
+        window.addEventListener('scroll', ()=>{
+            scrollPosition = window.scrollY;
+
+            if (scrollPosition >= 100) {
+                nav.classList.add('colored');
+            } else {
+                nav.classList.remove('colored');
+            }
+        });
+
+        
+        
     });/*  */
 
+
+    function mobileMenu(){
+        event.preventDefault();
+        var nav = document.getElementById('mobileNav');
+        if(nav == null){
+            fetch("/html/template/mobile_menu.html")
+                .then(function (response) {
+                    return response.text()
+                })
+                .then(function (data) {
+                    document.querySelector("header").insertAdjacentHTML( 'beforeend',data);
+                });
+        }else{
+            nav.remove();
+        }
+
+    }
